293c4743cb1024b7dc5e9f90d311b97303c9f2f4
Shejan-tamim <shejantamim@gmail.com>
2024-07-07 19:30:06 +0600

    assignment one

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,12 @@
+
+<?php
+// Temperature in Celsius
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Output the temperature in Fahrenheit
+echo "32 degrees Celsius is equal to $fahrenheit degrees Fahrenheit.";
+?>
+
